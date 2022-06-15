6b1df490703db7df166ea8dfee5de2cb20167bed
Roman Rodin <romaro83@gmail.com>
2022-06-16 00:45:52 +0300

    add comment

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-import Handlebars from 'handlebars';
-import { readFile } from 'node:fs/promises';
+import Handlebars from "handlebars";
+import { readFile } from "node:fs/promises";
 
-// Returns a function, but not fill placeholders
+// Return a function, but not fill placeholders
+// Ask Handlebars to INDEX template
 function newTemplate(source) {
-	return Handlebars.compile(source);
+  return Handlebars.compile(source);
 }
 
 async function render(path, placeholders) {
-	try {
-		// If you passed encoding, readFile returns Buffer
-		const source = await readFile(path, { encoding: 'utf-8' });
+  try {
+    // If 'encoding' will be passed, readFile returns Buffer
+    const source = await readFile(path, { encoding: "utf-8" });
 
-		return newTemplate(source)
-			.call(null, placeholders);
-	} catch (err) {
-		console.log(err.message);
-	}
+    return newTemplate(source).call(null, placeholders);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-const fileDir = '/home/romaro/handlebars/templates/';
-const fileName = 'index.html';
+const fileDir = "/home/romaro/handlebars/templates/";
+const fileName = "index.html";
 
 const path = fileDir + fileName;
 const placeholders = {
-	username: 'Roma'
+  username: "Roma",
 };
 render(path, placeholders)
-	.then(rs => console.log(rs))
-	.catch(err => console.log(err.message));
+  .then((rs) => console.log(rs))
+  .catch((err) => console.log(err.message));
